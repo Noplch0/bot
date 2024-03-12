f3ca77d29b8cd0f1310cba79f8b1e805205f684c
Noplch <a2579950433@outlook.com>
2024-03-12 15:25:57 +0800

    init

--- modules/pixiv/__init__.py
+++ modules/pixiv/__init__.py
@@ -1,44 +1,8 @@
-from graia.ariadne.app import Ariadne
-import pkgutil
-from graia.ariadne.entry import config
-from graia.ariadne.event.message import GroupMessage
-from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.model import Group
-from graia.saya import Saya
-from creart import create
-from graia.ariadne.app import Ariadne
-from graia.ariadne.connection.config import (
-    HttpClientConfig,
-    WebsocketClientConfig,
-    config,
-)
+from nonebot import on_command
+from nonebot.adapters.qq import Message, MessageSegment, MessageEvent
+from nonebot.params import CommandArg
 import requests
 import os
-
-saya=create(Saya)
-
-app = Ariadne(
-    config(
-        verify_key="lkijnfgh",  # 填入 VerifyKey
-        account=3079809050,  # 你的机器人的 qq 号
-        # 以下两行（不含注释）里的 host 参数的地址
-        # 是你的 mirai-api-http 地址中的地址与端口
-        # 他们默认为 "http://localhost:8080"
-        # 如果你 mirai-api-http 的地址与端口也是 localhost:8080
-        # 就可以删掉这两行，否则需要修改为 mirai-api-http 的地址与端口
-        #HttpClientConfig(host="localhost:8080"),
-        #WebsocketClientConfig(host="localhost:8080"),
-    ),
-)
-
-with saya.module_context():
-    for module_info in pkgutil.iter_modules(["modules"]):
-        if module_info.name.startswith("_"):
-            # 假设模组是以 `_` 开头的，就不去导入
-            # 根据 Python 标准，这类模组算是私有函数
-            continue
-        saya.require(f"modules.{module_info.name}")
-
 
 pixiv = on_command("蓝p", aliases={"pixiv", "pid"})
 
@@ -70,7 +34,7 @@ def getpic(pid, geshi='jpg'):
 
 @pixiv.handle()
 async def _(event: MessageEvent, args: Message = CommandArg()):
-    path = "./savedpic"
+    path = "savedpic"
     if pid := args.extract_plain_text():
         result = getpic(pid, geshi='jpg')
         if result == -1:
@@ -84,5 +48,3 @@ async def _(event: MessageEvent, args: Message = CommandArg()):
             for i in file_list:
                 image = MessageSegment.file_image(f'./savedpic/{pid}/{i}')
                 await pixiv.send(image)
-
-app.launch_blocking()
