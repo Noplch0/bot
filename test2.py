e72edfb5f1a7f70ec33c2ad1433a8c4bd3eb42c0
Noplch <a2579950433@outlook.com>
2024-04-23 09:39:34 +0800

    删除冗余文件

--- test2.py
+++
@@ -1,7 +0,0 @@
-from mybotlib.config import *
-from fflogsapi import FFLogsClient
-from test import *
-fapi = FFLogsClient(CLIENT_ID, CLIENT_SECRET)
-#print(character.encounter_rankings({"encounterID":1070}))
-player=PlayerInf("Storiasd","琥珀原")
-print(player.isexist)
