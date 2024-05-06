e36129505a35c0595394281fe93fe444ab63f0de
Noplch <a2579950433@outlook.com>
2024-05-06 14:02:50 +0800

    .

--- mybotlib/check.py
+++ mybotlib/check.py
@@ -6,21 +6,26 @@ import os
 class BotConfig:
     def read(self,data):
         self.data=self.loader.load(data)
+        
     def __init__(self,filepath='botconfig.yaml') -> None:
         with open(filepath,'r',encoding='utf-8') as f:
             data=f.read()
         self.path=filepath
         self.loader=yaml.YAML(typ='rt')
         self.read(data=data)
+        
     def save(self,path=None):
         with open((self.path if not path else path),'w',encoding='utf-8') as f:
             self.loader.dump(self.data,f)
+            
     def getstring(self):
         with open(self.path,'r',encoding='utf-8') as f:
             return f.read()
+        
     def backup(self):
         with open(self.path+'.backup','w',encoding='utf-8') as f:
             self.loader.dump(self.data,f)
+            
     def delitem(self,index:list):
         def delitembyindex(data:CommentedMap,index:list):
             if len(index)==1:
@@ -40,5 +45,4 @@ def notcom(text):
     return False if text[0]!=' ' else True
 
 def checksuf(text):
-    #print(fr"'{text}',{len(text)}")
     return True if len(text)>0 else False
